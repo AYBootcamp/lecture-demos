acb78678656d1ecaffcc3141268f34d9d3e141f9
guohao.yan <sccdmaomao@hotmail.com>
2022-11-12 12:12:13 -0500

    add react state demo

---
+++ react-state-demo/src/components/FunctionalComponent.js
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+
+const FC = () => {
+
+    // const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            Functional Component
+            {/* <span>current count is: {count}</span>
+            <div>
+                <button onClick={() => setCount(count + 1)}>increase count</button>
+                <button onClick={() => setCount(count - 1)}>decrease count</button>
+            </div> */}
+        </div>
+    )
+}
+
+export default FC
