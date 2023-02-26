bf2ae86b8b4b91f424d6fc19300f608bd98681ec
guohao.yan <sccdmaomao@hotmail.com>
2023-02-25 21:46:14 -0500

    add MUI component to storybook

---
+++ storybook/src/stories/MUIComponents.stories.jsx
@@ -0,0 +1,46 @@
+// Import Fonts
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import Button from "@mui/material/Button";
+import React from "react";
+import styled from "styled-components";
+
+export default {
+  title: "MUIComponents/Button",
+  component: Button,
+  argTypes: {
+    onClick: {
+      control: "func",
+    },
+  },
+};
+
+// Controlled Element
+const Template = (args) => {
+  return <Button {...args}>A MUI Button!</Button>;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Contained = Template.bind({});
+Contained.args = {
+  variant: "contained",
+};
+
+export const Outlined = Template.bind({});
+Outlined.args = {
+  variant: "outlined",
+};
+
+const StyledMUIButton = styled(Button)`
+  background-color: orange !important; // we need to override the styles.
+`;
+
+export const Styled = (args) => (
+  <StyledMUIButton {...args}>A Custom Styled MUI Button!</StyledMUIButton>
+);
+Styled.args = {};
